538b54d769110cce1cf08a91fb5c61361a6b2995
eboye <eboyee@gmail.com>
2021-09-06 04:48:02 +0200

    python 3 conversion

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ from freesteel.eid_card import CardFile, EidCard, PersonalField
 reader = get_default_reader()
 
 # wait for card insertion
-print 'Waiting for card in "{0}"...'.format(reader.name)
+print('Waiting for card in "{0}"...'.format(reader.name))
 card = reader.wait_for_card()
 
-print "Card inserted!"
+print("Card inserted!")
 
 # assume it's electronic ID
 eid_card = EidCard(card)
@@ -21,18 +21,18 @@ eid_card = EidCard(card)
 personal = eid_card.get_personal()
 photo = eid_card.get_photo()
 
-print 'Card owner is: {0} ({1}) {2}'.format(
+print('Card owner is: {0} ({1}) {2}'.format(
 										personal[PersonalField.FIRST_NAME], 
 										personal[PersonalField.MIDDLE_NAME], 
 										personal[PersonalField.LAST_NAME]
-									)
+									))
 
 # save photo to file
-jpeg_file = open('photo.jpg', 'wb+')
+jpeg_file = open('photo.jpg', 'w+')
 jpeg_file.write(photo)
 jpeg_file.close()
 
-print 'Photo was saved in: photo.jpg'
+print('Photo was saved in: photo.jpg')
 
 card.disconnect()
 reader.release_context()
